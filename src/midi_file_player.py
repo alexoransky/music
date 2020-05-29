8a435a8d458527fe492c45239ac4dc1d4a64a63f
Alex Oransky <alexoransky@gmail.com>
2020-05-29 16:01:59 -0400

    MIDIFile: Add track_message_cnt(). MIDIFilePLayer: Reworked _open_ports() to return a list of ports.

--- src/midi_file_player.py
+++ src/midi_file_player.py
@@ -5,19 +5,27 @@ from mido import MidiFile
 from midi_router import MIDIRouter, MIDIPort
 
 
+THREAD_PERIOD_MS = 50
+
+
 class MIDIFile(MidiFile):
     def __init__(self, path: str):
         self.path = str(Path(path).absolute().resolve())
         super().__init__(self.path, clip=True)
-        self.total_msg_cnt = self._get_total_message_count()
+        self.total_message_cnt = self.message_count()
 
-    def _get_total_message_count(self, include_meta=False):
+    def track_message_count(self, track, include_meta=False):
+        cnt = 0
+        for msg in track:
+            if msg.is_meta and not include_meta:
+                continue
+            cnt += 1
+        return cnt
+
+    def message_count(self, include_meta=False):
         cnt = 0
         for track in self.tracks:
-            for msg in track:
-                if msg.is_meta and not include_meta:
-                    continue
-                cnt += 1
+            cnt += self.track_message_count(track, include_meta=include_meta)
         return cnt
 
 
@@ -26,37 +34,35 @@ class MIDIFilePlayer:
         self._synth = synth
         self._port_mask = port_mask
         self._file = None
-        self._ports = []
+        self._ports = None
         self._active = False
         self._paused = False
         self._curr_msg_cnt = 0
         self._thread = None
-        self._thread_period_ms = 50
+        self._thread_period = THREAD_PERIOD_MS * 0.001
 
     def _open_ports(self, max_count: int = 0):
-        self._ports.clear()
+        """
+        Tries to open the requested number of ports
+        :param max_count: the requested number of ports to open.  If 0, tries to open all
+        :return: a list of opened ports
+        """
+        ports = []
         out_ports = MIDIRouter.available_ports(output=True)
-        cnt = 0
         for port_name in out_ports:
             if self._port_mask in port_name:
                 port = MIDIPort()
                 port.open(port_name)
                 if not port.is_open():
-                    return False
+                    continue
 
-                self._ports.append(port)
-                cnt += 1
+                ports.append(port)
+
             if max_count > 0:
-                if cnt >= max_count:
+                if len(ports) >= max_count:
                     break
 
-        if cnt == 0 or cnt < max_count:
-            return False
-
-        if len(self._ports) < 1:
-            return False
-
-        return True
+        return ports
 
     def _close_ports(self):
         for port in self._ports:
@@ -70,11 +76,20 @@ class MIDIFilePlayer:
         self._file = None
 
     def start(self, max_channel_cnt=1):
-        port_cnt = min(len(self._file.tracks), max_channel_cnt)
+        port_cnt = 0
+        for track in self._file.tracks:
+            if self._file.track_message_count(track) > 0:
+                port_cnt += 1
+
+        if port_cnt < 1:
+            return False
+
+        port_cnt = min(port_cnt, max_channel_cnt)
         if port_cnt == 0:
             port_cnt = 1
 
-        if not self._open_ports(max_count=port_cnt):
+        self._ports = self._open_ports(port_cnt)
+        if len(self._ports) == 0:
             return False
 
         for ch in range(port_cnt):
@@ -92,7 +107,7 @@ class MIDIFilePlayer:
         msgs = self._file.play()
         while self._active:
             if self._paused:
-                time.sleep(self._thread_period_ms * 0.001)
+                time.sleep(self._thread_period)
                 continue
 
             try:
@@ -126,7 +141,7 @@ class MIDIFilePlayer:
         return self._paused
 
     def total_msg_cnt(self):
-        return self._file.total_msg_cnt
+        return self._file.total_message_cnt
 
     def curr_msg_cnt(self):
         return self._curr_msg_cnt
@@ -137,7 +152,6 @@ class MIDIFilePlayer:
     @classmethod
     def print(cls, path: str, meta=True, notes=False):
         file = MidiFile(str(Path(path).absolute().resolve()))
-
         print(file)
         for i, track in enumerate(file.tracks):
             print('Track {}: {}'.format(i, track.name))
